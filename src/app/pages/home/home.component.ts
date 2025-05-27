285c556ef7f2bd3cb3440884ce4446510fd3898c
eriksmedeiros <erik.medeiros.701@ufrn.edu.br>
2025-05-27 08:14:55 -0300

    feat: home service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,46 @@
+import { HomeService } from './../services/home.service';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+    taskObj: any = {
+        title: '',
+        status: ''
+    };
+
+    todoForm: FormGroup;
+    tasks: any[] = [];
+
+    constructor(private formBuilder: FormBuilder,
+                private homeService: HomeService)
+    {
+        this.todoForm = this.formBuilder.group({
+            title: [''],
+            status: ['pending']
+        });
+    }
+
+    getTasks(): void {
+
+    }
+
+    addTask(): void {
+        const task = this.todoForm.value;
+        if (task) {
+            this.taskObj = {
+                title: task.title,
+                status: task.status
+            };
+            this.tasks.push(this.taskObj);
+            this.todoForm.reset();
+        }
+    }
 }
---
+++ src/app/pages/services/home.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  private apiUrl = 'http://localhost:8080/tasks';
+
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  addTask(task: any): Observable<any> {
+    return this.http.post(this.apiUrl, task);
+  }
+}
