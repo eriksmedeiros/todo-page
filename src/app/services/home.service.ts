5c4908045c23bc81d390ed11d7c87a74a0ce05cd
eriksmedeiros <erik.medeiros.701@ufrn.edu.br>
2025-06-07 00:34:34 -0300

    add environment.ts

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -2,12 +2,13 @@ import { Task } from './../models/task';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
-  private apiUrl = '/api/tasks';
+  private apiUrl = `${environment.apiUrl}/tasks`;
 
   constructor(private http: HttpClient) {}
 
